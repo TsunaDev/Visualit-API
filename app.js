d62ca09d15a8625e1883cbba4d283867804c8764
Martin Van Elslande <martin.van-elslande@epitech.eu>
2020-01-04 19:54:26 +0900

    Ajout de la possibilité de faire des requêtes sur l'API depuis n'importe quel domaine

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ const userRouter = require('./routes/user');
 
 const app = express();
 
+// Add CORS handling (any origin)
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
